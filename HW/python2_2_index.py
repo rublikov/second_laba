fc79822b205d541d509bf1e69a930eeaa6160b06
rublikov <ivan.rublikov@yandex.ru>
2021-09-30 22:30:35 +0300

    changed index

--- HW/python2_2_index.py
+++ HW/python2_2_index.py
@@ -8,81 +8,122 @@ def line(x1, x2, y2):
     turtle.pendown()
     turtle.goto(turtle.pos()+ (x2, y2))
     turtle.penup()
-a=60
-d={'1': (turtle.penup(),
-         turtle.goto(turtle.pos()+(0, a)),
-         line(turtle.pos(), a, a),
-         line(turtle.pos(), 0, -2*a),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+(a/4, 0))),
-   '0': (line(turtle.pos(), 0, a+a),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), -0, -2*a),
-         line(turtle.pos(), -a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a*1.25, 0))),
-   '2': (turtle.penup(),
-         turtle.goto(turtle.pos()+ (0, 2*a)),
-         turtle.pendown(),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), -0, -a),
-         line(turtle.pos(), -a, -a),
-         line(turtle.pos(), a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a/4, 0))),
-   '4': (turtle.penup(),
-         turtle.goto(turtle.pos()+ (0, 2*a)),
-         turtle.pendown(),
-         line(turtle.pos(), 0, -a),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), 0, a),
-         line(turtle.pos(), 0, -2*a),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a/4, 0))),
-   '3': (line(turtle.pos(), a, a),
-         line(turtle.pos(), -a, 0),
-         line(turtle.pos(), a, a),
-         line(turtle.pos(), -a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a*1.25, -a-a))),
-   '5': (line(turtle.pos(), a, 0),
-         line(turtle.pos(), 0, a),
-         line(turtle.pos(), -a, 0),
-         line(turtle.pos(), 0, a),
-         line(turtle.pos(), a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a/4, -2*a))),
-   '6': (turtle.penup(),
-         turtle.goto(turtle.pos()+ (a, 2*a)),
-         turtle.pendown(),
-         line(turtle.pos(), -a, -a),
-         line(turtle.pos(), -0, -a),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), 0, a),
-         line(turtle.pos(), -a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a*1.25, -a))),
-   '7': (line(turtle.pos(), 0, a),
-         line(turtle.pos(), a, a),
-         line(turtle.pos(), -a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a*1.25, -2*a))),
-   '8': (line(turtle.pos(), 0, 2*a),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), 0, -2*a),
-         line(turtle.pos(), -a, 0),
-         line(turtle.pos(), 0, a),
-         line(turtle.pos(), a, 0),
-         line(turtle.pos(), 0, -a),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a/4, 0))),
-   '9': (line(turtle.pos(), a, a),
-         line(turtle.pos(), -0, a),
-         line(turtle.pos(), -a, 0),
-         line(turtle.pos(), 0, -a),
-         line(turtle.pos(), a, 0),
-         turtle.penup(),
-         turtle.goto(turtle.pos()+ (a/4, -a))),
-   }
-d['1']
+a=50
+def one():
+    turtle.penup(),
+    turtle.goto(turtle.pos()+(0, a)),
+    line(turtle.pos(), a, a),
+    line(turtle.pos(), 0, -2*a),
+    turtle.penup(),
+    turtle.goto(turtle.pos()+(a/4, 0))
+    
+def zero():
+         line(turtle.pos(), 0, a+a)
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), -0, -2*a)
+         line(turtle.pos(), -a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a*1.25, 0))
+def two():
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (0, 2*a))
+         turtle.pendown()
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), -0, -a)
+         line(turtle.pos(), -a, -a)
+         line(turtle.pos(), a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a/4, 0))
+def four():
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (0, 2*a))
+         turtle.pendown()
+         line(turtle.pos(), 0, -a)
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), 0, -2*a)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a/4, 0))
+def three():
+         line(turtle.pos(), a, a)
+         line(turtle.pos(), -a, 0)
+         line(turtle.pos(), a, a)
+         line(turtle.pos(), -a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a*1.25, -a-a))
+def five():
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), -a, 0)
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a/4, -2*a))
+def six():
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a, 2*a))
+         turtle.pendown()
+         line(turtle.pos(), -a, -a)
+         line(turtle.pos(), -0, -a)
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), -a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a*1.25, -a))
+def seven():
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), a, a)
+         line(turtle.pos(), -a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a*1.25, -2*a))
+def eight():
+         line(turtle.pos(), 0, 2*a)
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), 0, -2*a)
+         line(turtle.pos(), -a, 0)
+         line(turtle.pos(), 0, a)
+         line(turtle.pos(), a, 0)
+         line(turtle.pos(), 0, -a)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a/4, 0))
+def nine():
+         line(turtle.pos(), a, a)
+         line(turtle.pos(), -0, a)
+         line(turtle.pos(), -a, 0)
+         line(turtle.pos(), 0, -a)
+         line(turtle.pos(), a, 0)
+         turtle.penup()
+         turtle.goto(turtle.pos()+ (a/4, -a))
+d={'1': (one()),
+   "2": (two()),
+   "3": (three()),
+   "4": (four()),
+   "5": (five()),
+   "6": (six()),
+   "7": (seven()),
+   "8": (eight()),
+   "9": (nine()),
+   "0": (zero()),
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
